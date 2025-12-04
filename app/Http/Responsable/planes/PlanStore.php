1da8eeda79ee004d91f12e7c76bfb87fe452e24a
Juangui <jgmejiaco@gmail.com>
2025-12-04 12:07:31 -0500

    store de planes

--- app/Http/Responsable/planes/PlanStore.php
+++ app/Http/Responsable/planes/PlanStore.php
@@ -4,28 +4,24 @@ namespace App\Http\Responsable\planes;
 
 use Exception;
 use Illuminate\Contracts\Support\Responsable;
-use App\Models\Suscripcion;
+use App\Models\Plan;
 
 class PlanStore implements Responsable
 {
     public function toResponse($request)
     {
         try {
-            $nuevaSuscripcion = Suscripcion::create([
-                'id_empresa_suscrita'       => $request->input('id_empresa_suscrita'),
-                'id_plan_suscrito'          => $request->input('id_plan_suscrito'),
-                'dias_trial'                => $request->input('dias_trial'),
-                'id_tipo_pago_suscripcion'  => $request->input('id_tipo_pago_suscripcion'),
-                'valor_suscripcion'         => $request->input('valor_suscripcion'),
-                'fecha_inicial'             => $request->input('fecha_inicial'),
-                'fecha_final'               => $request->input('fecha_final'),
-                'id_estado_suscripcion'     => $request->input('id_estado_suscripcion'),
-                'fecha_cancelacion'         => $request->input('fecha_cancelacion'),
-                'renovacion_automatica'     => $request->input('renovacion_automatica', 0), // Default 0
-                'observaciones_suscripcion' => $request->input('observaciones_suscripcion'),
+            $nuevoPlan = Plan::create([
+                'nombre_plan' => $request->input('nombre_plan'),
+                'valor_mensual' => $request->input('valor_mensual'),
+                'valor_trimestral' => $request->input('valor_trimestral'),
+                'valor_semestral' => $request->input('valor_semestral'),
+                'valor_anual' => $request->input('valor_anual'),
+                'descripcion_plan' => $request->input('descripcion_plan'),
+                'id_estado_plan' => $request->input('id_estado_plan')
             ]);
 
-            if ($nuevaSuscripcion) {
+            if ($nuevoPlan) {
                 return response()->json(['success' => true]);
             }
 
